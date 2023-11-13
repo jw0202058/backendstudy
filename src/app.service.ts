7c6c7f2f680e720919fe219236a6422946a31c66
jw0202058 <jw0202058@korea.ac.kr>
2023-11-13 20:52:45 +0900

    docs: docker

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,18 +3,34 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   usernames: string[];
-  constructor(){
-    this.usernames=['노정훈', '권예진'];
+  constructor() {
+    this.usernames = ['노정훈', '권예진', '이정원'];
   }
 
-  getHello(): string{
+  getHello(): string {
     return 'Hello World!';
   }
 
-  getUsernameByIndex(index: number){
-    if(index<0||index>= this.usernames.length){
+  getUsernameByIndex(index: number) {
+    if (index < 0 || index >= this.usernames.length) {
       throw Error('Index out of range');
     }
-    return this.usernames[index]
+    return this.usernames[index];
+  }
+
+  postUsername(name: string) {
+    if (name == null) {
+      throw Error('Invalid User Name');
+    }
+    this.usernames.push(name);
+    return this.usernames;
+  }
+
+  deleteUsernameByIndex(index: number) {
+    if (index < 0 || index >= this.usernames.length) {
+      throw Error('Index out of range');
+    }
+    this.usernames.splice(index, 1);
+    return this.usernames;
   }
 }
